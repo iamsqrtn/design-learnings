8d4a41a80526e9f82b1f6859b2860b6d95cba1f2
iamsqrtn <masterakg2000@gmail.com>
2025-01-27 11:38:49 +0530

    Commit for Singleton Design Pattern

--- DesignPatterns/src/pattern/creational/singleton/Client.java
+++ DesignPatterns/src/pattern/creational/singleton/Client.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Executors;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Singleton Design Pattern");
         ExecutorService executorService = Executors.newFixedThreadPool(4);
         for(int i=0;i<10;i++) {
             executorService.submit(()->{
